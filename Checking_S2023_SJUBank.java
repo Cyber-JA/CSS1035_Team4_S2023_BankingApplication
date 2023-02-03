abc735b2debe5e37fb3ef787eba6ca9579be43ef
GiuseppeCaruso <s303335@studenti.polito.it>
2023-02-03 11:32:03 -0500

    Changes
    -Renaming of classes according to the requirements
    -Main set up and comments

---
+++ Checking_S2023_SJUBank.java
@@ -0,0 +1,142 @@
+
+public class Checking_S2023_SJUBank extends Account_S2023_SJUBank {
+	/*Starting Balance of the Checking account*/
+	//thanks to the superclass, balance is available //private double Balance;
+	/*Overdraft is possible but only once. To perform a new one it is needed to have a positive balance*/
+	private double overdraftcounter;
+	/*A fee is applied when overdraft*/
+	private double overdraftFee;
+	/*ATM withdrawal fee*/
+	private double WithdrawalsFee;
+
+	/*Constructors*/
+	Checking_S2023_SJUBank() {
+		this.setBalance(0);
+		this.setOverdraftcounter(0);
+		this.setOverdraftFee(2);
+		this.setWithdrawalsFee(1);
+		System.out.println("Checking account created. Current balance: " + this.getBalance());
+	}
+
+	Checking_S2023_SJUBank(double balance) {
+		this.setBalance(balance);
+		this.setOverdraftcounter(0);
+		this.setOverdraftFee(2);
+		this.setWithdrawalsFee(1);
+		System.out.println("Checking account created. Current balance: " + this.getBalance());
+	}
+	/*************/
+	
+	/*overdraftFee Getter and Setter*/
+	public double getOverdraftFee() {
+		return overdraftFee;
+	}
+
+	public void setOverdraftFee(double overdraftFee) {
+		this.overdraftFee = overdraftFee;
+	}
+	/*********************************/
+	
+	/*Overdraftcounter getter and setter*/
+	public double getOverdraftcounter() {
+		return overdraftcounter;
+	}
+
+	public void setOverdraftcounter(double overdraftcounter) {
+		this.overdraftcounter = overdraftcounter;
+	}
+	/***********************************/
+	 
+	/*WithdrawalsFee getter and setter*/  
+	public double getWithdrawalsFee() {
+		return WithdrawalsFee;
+	}
+
+	public void setWithdrawalsFee(double withdrawalsFee) {
+		WithdrawalsFee = withdrawalsFee;
+	}
+	/**********************************/
+	
+	/*Method to apply withdrawals fee*/
+	public void applyWithdrawalsFee() {
+		 System.out.println("Applying withdrawals fee (" + this.getOverdraftFee() + "$).");
+		 Balance = Balance - this.getOverdraftFee();
+		 System.out.println("Current balance after withdrawal: " + this.getBalance());
+	}
+	
+	/*Method to apply overdraft fee*/
+	public void applyOverdraftFee() {
+		 System.out.println("Applying overdraft fee (" + this.getWithdrawalsFee() + "$).");
+		 Balance = Balance - this.getWithdrawalsFee();
+		 System.out.println("Current balance after overdraft: " + this.getBalance());
+	}
+
+
+  /** Withdraw method*/
+  @Override
+ public void withdraw(double amount) {
+	  System.out.println("Withdrawing...");
+	 if(Balance-amount<0 && overdraftcounter==0) {  // checking if account will be overdrawn by withdrawal
+		 
+		 //final double fee = 2; //fee imposed to the first overdraft transaction  
+		 System.out.println("Insufficient funds, a two dollar fee will be levied.");
+		 Balance = Balance - amount; // first overdraft transaction is allowed but a two dollar fee is imposed.
+		 this.applyWithdrawalsFee();
+		 this.applyOverdraftFee();
+		 overdraftcounter++; //mark the overdraft
+		  
+	  }
+	 
+	  else if(Balance-amount>=0 && overdraftcounter==0) { //if not overdraft, proceed with the withdrawal
+		  System.out.println("Withdrawn amount: " + amount);
+		  Balance = Balance - amount;
+		  this.applyWithdrawalsFee();
+		  if(this.getBalance() < 0) {
+			  this.applyOverdraftFee();
+			  this.overdraftcounter++;
+			  
+		  }
+	  
+	  }
+	  else if(overdraftcounter!=0) { //last possible case, if overdraft, cannot proceed with withdrawal
+		  
+		  System.out.println("Your account is overdrafted. Please restore your account to a positive balance to make another withdrawal.");
+	  
+	  }
+}
+ /** Deposit method*/
+  @Override
+ public void deposit(double amount) {
+	 System.out.println("Depositing...");
+	 Balance = Balance + amount;
+	 System.out.println("Deposited amount: " + amount);
+	 if(Balance>=0) { //
+		 if(this.getOverdraftcounter() > 0)
+			 System.out.println("Overdraft restored.");
+		 this.setOverdraftcounter(0);
+	 }
+ 
+ }
+  
+  /** Method to perform payment with checking account*/
+  public void makePayment(double amount) {
+	  
+	if(this.Balance - amount < 0 && this.getOverdraftcounter() != 0) {
+		System.out.println("Cannot process payment. Not enough founds:" + this.getBalance() + ". Amount to pay: " + amount);
+		return;
+	}
+	else if(this.Balance - amount < 0 && this.getOverdraftcounter() == 0) {
+		this.Balance -= amount;
+		System.out.println("Insufficient funds, a two dollar fee will be levied.");
+		this.applyOverdraftFee();
+		overdraftcounter++; //mark the overdraft
+		System.out.println("Payment completed. New balance: " + this.getBalance());
+		return;
+	}
+	else {
+		this.Balance -= amount;
+		System.out.println("Payment completed. New balance: " + this.getBalance());
+	}
+  }
+  
+}
--- bankDriver.java
+++ bankDriver.java
@@ -2,45 +2,71 @@
 public class bankDriver {
 
 	public static void main(String[] args) {
-		// arraylist of bank accounts
-		// arraylist of savings accounts
 		
-		Checking_S2023_TEAM4 acc1 = new Checking_S2023_TEAM4(100);
-		//acc1.deposit(100);
-		acc1.withdraw(100);  // testing methods
-		//System.out.println(acc1.getBalance());
-		acc1.withdraw(100.1);
-		acc1.deposit(1);
-		//System.out.println(acc1.getBalance());
-		acc1.withdraw(.1);
-		//System.out.println(acc1.getBalance());
-		acc1.deposit(1000);
-		System.out.println(acc1.getBalance());
-		acc1.withdraw(500);
-		acc1.makePayment(200);
-		acc1.makePayment(200);
-		acc1.makePayment(200);
-		acc1.deposit(8);
-		System.out.println(acc1.getBalance());
-		acc1.withdraw(20);
-		System.out.println(acc1.getBalance());
-		acc1.makePayment(0);
+		/********************CHECKING ACCOUNT TESTING********************/
+		/*a checking account is created with an initial deposit of 102$*/
+		Checking_S2023_SJUBank acc1 = new Checking_S2023_SJUBank(102);
+		System.out.println();
+		
+		/*attempt to withdraw 100$, this should succeed. A fee (2$) is applied for withdrawals*/
+		acc1.withdraw(100);  //current balance should be: 0$
+		System.out.println();
+		
+		/*attempt to withdraw 100$, this should succeed but overdraft the account. A fee is applied for 
+		 * withdrawals and for overdraft*/
+		acc1.withdraw(100);  //current balance should be: -103$
+		System.out.println();
+		
+		/*attempt to withdraw 1$, this should not succeed because of overdraft*/
+		acc1.withdraw(1);  
+		System.out.println();
+		
+		/*testing deposit method*/
+		acc1.deposit(1000); //this should restore the overdraft as well
+		System.out.println();
+		
+		/*testing deposit method, without overdraft*/
+		acc1.deposit(10);
+		System.out.println();
+		
+		/*testing makePayment method*/
+		acc1.makePayment(100); //new balance should be 807$
+		acc1.makePayment(807); //new balance should be 0$
+		/*attempting to overdraft*/
+		acc1.makePayment(1); //overdraft and payment are charged
+		
 		System.out.println("\n");
+		/****************END CHECKING ACCOUNT TESTING******************/
 		
-		Savings_S2023_TEAM4 saving = new Savings_S2023_TEAM4(0);
+		/********************SAVING ACCOUNT TESTING********************/
+		
+		/*a saving account is created with an initial deposit of 0$*/
+		Savings_S2023_SJUBank saving = new Savings_S2023_SJUBank(0);
+		System.out.println();
+		
+		/*try to withdraw, it fails because can't overdraft*/
 		saving.withdraw(100);
+		System.out.println();
+		
+		//depositing
 		saving.deposit(400);
-		saving.computeAccruedInterest();
-		saving.withdraw(100);
-		saving.withdraw(100);
-		saving.withdraw(100);
-		saving.withdraw(0);
-		saving.deposit(1000);
-		System.out.println(saving.getWithdrawalsAvailable());
-		saving.withdraw(10);
-		System.out.println(saving.getBalance());
-		saving.computeAccruedInterest();
+		System.out.println();
 		
+		/*testing computeAccruedInterest method, it shows a forecast interest with current balance amount*/
+		saving.computeAccruedInterest();
+		System.out.println();
+		System.out.println("Withdrawals available:" + saving.getWithdrawalsAvailable());
+		
+		//with this account, only a limited number of withdrawals is available
+		/*running out the withdrawals available*/
+		saving.withdraw(10);
+		System.out.println();
+		saving.withdraw(10);
+		System.out.println();
+		saving.withdraw(10); //this should not be permitted
+		System.out.println();
+		
+		/****************END SAVING ACCOUNT TESTING******************/
 	}
 
 }
