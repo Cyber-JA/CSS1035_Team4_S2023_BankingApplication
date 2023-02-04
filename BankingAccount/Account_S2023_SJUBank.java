5baa83b47af1684e1690db9fb54f7945067be0e2
HJM2 <124077975+HJM2@users.noreply.github.com>
2023-02-03 19:49:54 -0500

    Add files via upload
    Banking App plus JUnit test file.

---
+++ BankingAccount/Account_S2023_SJUBank.java
@@ -0,0 +1,41 @@
+package BankingAccount;
+public abstract class Account_S2023_SJUBank {
+ 
+    protected java.util.Date dateCreated;
+  	protected double Balance;
+ 
+	  protected Account_S2023_SJUBank() {
+	    dateCreated = new java.util.Date();
+	    this.Balance = 0;
+	  }
+	
+	 protected Account_S2023_SJUBank(double balance) {
+		this.dateCreated = new java.util.Date();
+		Balance = balance;
+	}
+
+	public double getBalance() {
+		return Balance;
+	}
+
+	public void setBalance(double balance) {
+		Balance = balance;
+	}
+
+	/** Get dateCreated */
+	  public java.util.Date getDateCreated() {
+	    return dateCreated;
+	  }
+	
+	  @Override
+	  public String toString() {
+	    return "created on " + dateCreated;
+	  }
+	
+	  /** Abstract method withdraw */
+	  public abstract void withdraw(double amount);
+	  
+	  /** Abstract method deposit */
+	  public abstract void deposit(double amount);
+
+}
---
+++ BankingAccount/BankTest.java
@@ -0,0 +1,15 @@
+package BankingAccount;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BankTest {
+
+	@Test
+	void test() {
+		Checking_S2023_SJUBank acc1 = new Checking_S2023_SJUBank(102);
+		assertEquals(101,acc1.getBalance());
+	}
+
+}
