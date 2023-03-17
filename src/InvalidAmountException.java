eefb9d5210fd8539d9b3cfff442b80051a10d50c
HJM2 <124077975+HJM2@users.noreply.github.com>
2023-03-17 18:44:33 -0400

    Add files via upload

---
+++ src/InvalidAmountException.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * This exception was defined to manage issues with negative inputs for the
+ * "withdraw" and "deposit" methods. A negative amount is not accepted, hence is
+ * managed by throwing such exception.
+ * 
+ * SECURE PRACTICE: This exception is used to prevent the user from inserting values that would
+ * trigger number overflows.
+ */
+public class InvalidAmountException extends Exception {
+ 
+  private static final long serialVersionUID = 1L;
+  
+  /**
+   * Amount that generated the exception.
+   */
+  double amount;
+
+  InvalidAmountException(double amount) {
+    System.out.println("Invalid amount inserted: ." + amount);
+    this.amount = amount;
+  }
+}
